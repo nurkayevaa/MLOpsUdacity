2a27cceb2e9aa21825b5eb343beca67668afd87e
Anel Nurkayeva <a.nurkayeva@gmail.com>
2022-03-28 21:27:08 -0700

    Update train.py

--- lesson3_pipeline/train.py
+++ lesson3_pipeline/train.py
@@ -1,8 +1,13 @@
+import yaml
+from yaml import CLoader as Loader
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score
+
+with open("./params.yaml", "rb") as f:
+    params = yaml.load(f, Loader=Loader)
 
 X = np.loadtxt("X.csv")
 y = np.loadtxt("y.csv")
@@ -11,7 +16,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.25, random_state=23
 )
 
-lr = LogisticRegression(C=1.0)
+lr = LogisticRegression(C=params["C"])
 lr.fit(X_train.reshape(-1, 1), y_train)
 
 preds = lr.predict(X_test.reshape(-1, 1))
